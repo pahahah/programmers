9f7d1815b075b678039f307c121e84bbc26de0ca
soohyunan <shahn2525@gmail.com>
2022-04-27 12:37:40 +0200

    level1 모의고사, 신고결과받기

---
+++ src/bruteForce/모의고사.java
@@ -0,0 +1,48 @@
+package bruteForce;
+
+import java.util.*;
+
+public class 모의고사 {
+    public static int[] solution(int[] answers) {
+
+        int[] p1 = {1, 2, 3, 4, 5};
+        int[] p2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] p3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+
+
+
+        int [] scores = {0, 0, 0};
+        for (int i = 0; i < answers.length; i++ ){
+            if (p1[i % 5] == answers[i]){
+                scores[0]++;
+            }
+            if(p2[i % 8] == answers[i]){
+                scores[1]++;
+            }
+            if(p3[i% 10] == answers[i]){
+                scores[2]++;
+            }
+        }
+
+        int[] arr = new int[scores.length];
+        for (int i = 0; i < scores.length; i++){
+            arr[i] = scores[i];
+        }
+
+        Arrays.sort(arr);
+        ArrayList<Integer> ans = new ArrayList<>();
+        for(int i = 0; i < arr.length; i++){
+            if(scores[i] == arr[2]){
+                ans.add(i + 1);
+            }
+        }
+
+        int[] answer = new int[ans.size()];
+        for(int i = 0; i < ans.size(); i++){
+            answer[i] = ans.get(i);
+        }
+
+        return answer;
+    }
+
+}
---
+++ src/kbr2022/신고결과받기.java
@@ -0,0 +1,64 @@
+package kbr2022;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class 신고결과받기 {
+    public static int[] solution(String[] id_list, String[] report, int k) {
+        int[] answer = new int[id_list.length];
+        HashMap<String, Integer> reportedNumber = new HashMap<>();
+        HashMap<String, ArrayList<String>> reportList = new HashMap<>();
+        String[] distReport = Arrays.stream(report).distinct().toArray(String[]::new);
+        for (String rep : distReport){
+            String reporter = rep.substring(0,rep.lastIndexOf(" "));
+            String reportedId = rep.substring(rep.lastIndexOf(" ") + 1);
+            if(reportedNumber.containsKey(reportedId)){
+                reportedNumber.replace(reportedId, reportedNumber.get(reportedId) + 1);
+            }else {
+                reportedNumber.put(reportedId, 1);
+            }
+
+            if(reportList.containsKey(reporter)){
+                ArrayList<String> result = reportList.get(reporter);
+                result.add(reportedId);
+                reportList.replace(reporter, result);
+            }else {
+                ArrayList<String> result = new ArrayList<>();
+                result.add(reportedId);
+                reportList.put(reporter, result);
+            }
+        }
+        int size = 0;
+        for (String id : id_list){
+            if(reportList.containsKey(id)){
+                for(String temp : reportList.get(id)){
+                    if(reportedNumber.containsKey(temp)){
+                        if(reportedNumber.get(temp) >= k){
+                            answer[size]++;
+                        }
+                    }
+                }
+            }else {
+                answer[size] = 0;
+            }
+            size++;
+        }
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+        String[] id_list = {"muzi", "frodo", "apeach", "neo"};
+        String[] report = {"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"};
+        int k = 2;
+
+        int[] result = solution(id_list, report, k);
+        for (int i : result){
+            System.out.println(i);
+        }
+
+
+
+    }
+}
